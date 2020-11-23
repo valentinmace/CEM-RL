91a9f3ac77fa98a4a72f62ce756b469b8841dc04
Valentin Macé <v.mace@instadeep.com>
2020-11-23 21:30:15 +0100

    added pointmaze mujoco inertia

--- register_envs.py
+++ register_envs.py
@@ -14,3 +14,8 @@ def register_envs():
         max_episode_steps=3000,
         entry_point='environments.ant_maze.ant_maze:AntMaze',
     )
+
+    gym.envs.registration.register(
+        id="PointMazeInertiaMujoco-v0",
+        entry_point="environments.point_maze_mujoco.point_maze_inertia_mujoco:PointMazeInertiaMujoco"
+    )
